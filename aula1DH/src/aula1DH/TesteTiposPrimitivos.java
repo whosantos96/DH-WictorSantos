d0ba3e986669efd2af05ab53675dde805860caf5
whosantos96 <swictor96@gmail.com>
2021-09-28 17:05:50 -0300

    Projetos

---
+++ aula1DH/src/aula1DH/TesteTiposPrimitivos.java
@@ -0,0 +1,28 @@
+package aula1DH;
+
+public class TesteTiposPrimitivos {
+     
+	public static void main(String[] args) {
+		
+		System.out.println("Aula 01 - Hello World");
+		
+		TiposPrimitivos  tp = new TiposPrimitivos();		
+	
+		//Tipos Inteiros 
+		
+		System.out.println(tp.tipoByte);
+		System.out.println(tp.tipoShort);
+		System.out.println(tp.tipoInt);
+		System.out.println(tp.tipoLong);
+		
+		//Tipo Flutuantes 
+		
+		System.out.println(tp.tipoFloat);
+		System.out.println(tp.tipoDouble);
+		
+		//Tipos Textos 
+		
+		System.out.println(tp.tipoChar);
+		
+	}  
+}
---
+++ aula1DH/src/aula1DH/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+package aula1DH;
+
+public class TiposPrimitivos {
+
+   // Tipos númericos - Inteiros 
+	     
+	byte  tipoByte = 4;  // 0-9
+	short tipoShort = 20;
+	int   tipoInt = 1500;
+	long  tipoLong = 950000000000000L;
+	
+	// Tipos Flutuantes (Decimais)
+	
+	float tipoFloat = 7.5f;
+	double tipoDouble = 7.55555555555;
+	
+	//Tipos Textos
+
+	char tipoChar = 'F';
+	
+	
+	// *String não é tipo primitivo
+	String nome = "Wictor"; 
+	
+	
+	
+	
+}
